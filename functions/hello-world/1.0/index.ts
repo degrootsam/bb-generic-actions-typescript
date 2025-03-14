346c01760a1e42ae17fde0a396fe8f41c8557eca
Sam <sam.degroot@bettyblocks.com>
2025-03-14 21:34:55 +0100

    fix(helloworld): update export

--- functions/hello-world/1.0/index.ts
+++ functions/hello-world/1.0/index.ts
@@ -1,7 +1,11 @@
 // import { hello } from "../../utils/index";
 
+import { VariableMap, Record } from "../../utils/types";
+
 type FunctionInput = {
   name: string;
+  record?: Record<{ id: string }>;
+  variableMap?: VariableMap;
 };
 
 type FunctionOutput = {
