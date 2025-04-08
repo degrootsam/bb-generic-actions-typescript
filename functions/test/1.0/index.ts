cdcd05f1f7bddbc2a81b8f264b379e62be846c27
Sam <39275315+degrootsam@users.noreply.github.com>
2025-04-08 13:44:41 +0200

    ci: fix formatting loop bug

---
+++ functions/test/1.0/index.ts
@@ -0,0 +1,11 @@
+type FunctionInput = {};
+
+type FunctionOutput = {
+  result: undefined;
+};
+
+export default async function test({}: FunctionInput): Promise<FunctionOutput> {
+  return {
+    result: undefined,
+  };
+}
