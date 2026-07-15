26cf7bbbfb4c02f48367c731c78249b29f99ca2a
Sam <39275315+degrootsam@users.noreply.github.com>
2026-07-15 17:05:20 +0200

    feat: added a check for config.json

--- scripts/build-and-publish.ts
+++ scripts/build-and-publish.ts
@@ -1,7 +1,19 @@
 import { build } from "./build";
 import { publish } from "./publish";
+import { stat } from "fs/promises";
+import path from "path";
 
 async function main() {
+  try {
+    const configPath = path.join(process.cwd(), "config.json");
+    await stat(configPath);
+  } catch (err) {
+    console.error(
+      "Unable to publish functions: config.json is missing. Please create a config.json with:\n\t- 'applicationId'\n\t- 'host'.\n See: https://github.com/bettyblocks/cli/wiki/Functions:Advanced",
+    );
+    process.exit(1);
+  }
+
   await build();
   await publish();
 }
